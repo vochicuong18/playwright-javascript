9b91a6ea72033faeb965faa08412dfc637d0a176
vochicuong18 <vochicuong0108@gmail.com>
2024-06-26 18:13:34 +0700

    first js code

---
+++ tests/example.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Check title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
